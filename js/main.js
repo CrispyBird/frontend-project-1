48b26ec47ad5a215ff95e95b2b67f867f5078f73
Bruno Wolvetang <2104272@talnet.nl>
2024-11-15 17:19:18 +0100

    Blauwe bollen gefixt en Spelersnaam toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 /* Teamnaam */
-window.onload = function() {
-    const startButton = document.getElementById("startButton");
-    const nameInput = document.getElementById("name");
-    const teamName = document.getElementById("TeamName");
+const startButton = document.getElementById("startButton");
+const nameInput = document.getElementById("name");
+const teamName = document.getElementById("TeamName");
 
-    
+window.onload = function() {
     if (startButton && nameInput) {
         startButton.onclick = function() {
             const playerName = nameInput.value;
@@ -41,14 +40,40 @@ function startTimer() {
         
         if (tijd <= 0) {
             clearInterval(timer);
-            window.location.href = ""; /* Deze link naar de homepage kan je eraf halen */
+            window.location.href = ""; 
         }
     }, 1000);
 }
 
 startTimer();
 
-// http://127.0.0.1:5500/index.html */
+/* Spelersnaam */
+const player_Name = document.getElementById("playerName");
+
+if (startButton && nameInput) {
+    startButton.onclick = function() {
+        const playerName = nameInput.value;
+
+        if (playerName === "") {
+            alert("Vul een naam in om de quiz te starten.");
+        } else {
+            
+            sessionStorage.setItem("playername", playerName);
+            
+            
+            window.location.href = "http://127.0.0.1:5500/pages/themascherm.html";
+        }
+    };
+}
+
+
+const playerName = sessionStorage.getItem("playername");
+
+
+if (player_Name && playerName) {
+    player_Name.innerText = playerName;
+};
+
 
 
 
