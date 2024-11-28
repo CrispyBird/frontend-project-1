b56df3c8969be3e03e778004d24f568eb6179018
Tycho Bohm <2189905@talnet.nl>
2024-11-28 15:31:05 +0100

    json en javascript

--- js/main.js
+++ js/main.js
@@ -11,19 +11,14 @@ window.onload = function() {
             if (playerName === "") {
                 alert("Vul een naam in om de quiz te starten.");
             } else {
-                
                 sessionStorage.setItem("playername", playerName);
-                
-                
                 window.location.href = "http://127.0.0.1:5500/pages/themascherm.html";
             }
         };
     }
 
-    
     const playerName = sessionStorage.getItem("playername");
 
-    
     if (teamName && playerName) {
         teamName.innerText = "Team: " + playerName;
     }
@@ -37,10 +32,10 @@ function startTimer() {
     timer = setInterval(function() {
         tijd--;
         document.getElementById("timer").innerText = tijd;
-        
+
         if (tijd <= 0) {
             clearInterval(timer);
-            window.location.href = ""; 
+            window.location.href = ""; // Vervang met je gewenste actie
         }
     }, 1000);
 }
@@ -57,29 +52,60 @@ if (startButton && nameInput) {
         if (playerName === "") {
             alert("Vul een naam in om de quiz te starten.");
         } else {
-            
             sessionStorage.setItem("playername", playerName);
-            
-            
             window.location.href = "http://127.0.0.1:5500/pages/themascherm.html";
         }
     };
 }
 
-
 const playerName = sessionStorage.getItem("playername");
-
 
 if (player_Name && playerName) {
     player_Name.innerText = playerName;
-};
-
-
+}
 
 /* Input Characters */
-    const name_Input = document.getElementById('name');
-    const charCount = document.getElementById('charCount');
-
+const name_Input = document.getElementById('name');
+if (name_Input) {
     name_Input.setAttribute('maxlength', '8');
-    name_Input.addEventListener;
+    name_Input.addEventListener('input', () => {
+        document.getElementById('charCount').innerText = `${name_Input.value.length}/8`;
+    });
+}
 
+/* Quiz Logica */
+const thema1 = document.querySelector('.thema__1');
+const thema2 = document.querySelector('.thema__2');
+const thema3 = document.querySelector('.thema__3');
+const quizHeader = document.querySelector('h1');
+const quizButtons = document.querySelectorAll('.quiz-button');
+
+// Laad de JSON data
+let quizData;
+
+fetch('/path/to/quizvragen.json') // Verander dit pad naar je JSON-bestand
+    .then(response => response.json())
+    .then(data => {
+        quizData = data;
+    });
+
+function laadQuiz(thema) {
+    const vragen = quizData[thema];
+    const vraag = vragen[Math.floor(Math.random() * vragen.length)];
+
+    quizHeader.innerText = vraag.vraag;
+    quizButtons.forEach((button, index) => {
+        button.innerText = vraag.antwoorden[index];
+        button.onclick = () => {
+            if (index === vraag.correct) {
+                alert('Correct!');
+            } else {
+                alert('Fout, probeer opnieuw!');
+            }
+        };
+    });
+}
+
+if (thema1) thema1.onclick = () => laadQuiz('Dieren');
+if (thema2) thema2.onclick = () => laadQuiz('Software Development');
+if (thema3) thema3.onclick = () => laadQuiz('Landen');
